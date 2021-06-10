b10a7968e1295be0503571aafe6c84d10c7713b6
sudo-shaka <68864205+sudo-shaka@users.noreply.github.com>
2021-06-10 16:11:47 -0400

    Update Smooth_Boarders.m

--- matlab/Smooth_Boarders.m
+++ matlab/Smooth_Boarders.m
@@ -4,7 +4,6 @@ function c = Smooth_Boarders(cell)
 
 	angles = cell.angles;
 	linkers = cell.linkers;
-	%linkers(5,:) = smooth(linkers(5,:),5);
 	linkers(5,:) = filloutliers(linkers(5,:),'nearest');
 	linkers(3,:) = cos(angles).*linkers(5,:) + linkers(1,:);
 	linkers(4,:) = sin(angles).*linkers(5,:) + linkers(2,:);
